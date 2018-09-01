3f238bd8a0350d49808ad3224778a2eb80bcf97b
Yomar Miranda <mirandayomar@gmail.com>
2018-08-31 23:56:15 -0500

    Showing billboard movies, populars and kids movies.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../providers/movies.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html'
+    selector: 'app-home',
+    templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+    billboard: any;
+    populars: any;
+    kids: any;
 
-  constructor() { }
+    constructor(public _moviesService: MoviesService) {
+        this._moviesService.getBillboard().subscribe((data: any) => { this.billboard = data.results; });
+        this._moviesService.getPopulars().subscribe((data: any) => { this.populars = data.results; });
+        this._moviesService.getKidsMovies().subscribe((data: any) => { this.kids = data.results; });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
