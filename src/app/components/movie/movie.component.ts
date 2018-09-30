2a269336e7d9d0c295b176e62fb49be990090d2a
Yomar Miranda <mirandayomar@gmail.com>
2018-09-30 17:09:28 -0500

    Return to the search page with the name of the movie previously searched.

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -10,9 +10,13 @@ import { ActivatedRoute } from '@angular/router';
 export class MovieComponent implements OnInit {
   movie: any;
   pageOrigin = '';
+  movieName = '';
 
   constructor(public _movieService: MoviesService, public _activateRouter: ActivatedRoute) {
     this._activateRouter.params.subscribe(params => {
+      if (params['movieName']) {
+        this.movieName = params['movieName'];
+      }
       this.pageOrigin = params['page'];
       this._movieService.getMovie(params['id']).subscribe(movie => {
         this.movie = movie;
