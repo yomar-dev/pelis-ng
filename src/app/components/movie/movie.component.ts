cc94cde19a6ece66fe5e335322744b42f708edc8
Yomar Miranda <mirandayomar@gmail.com>
2018-09-30 16:28:24 -0500

    Get a specific movie.

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../providers/movies.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
@@ -6,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class MovieComponent implements OnInit {
+  movie: any;
 
-  constructor() { }
+  constructor(public _movieService: MoviesService, public _activateRouter: ActivatedRoute) {
+    this._activateRouter.params.subscribe(params => {
+      console.log(params);
+      this._movieService.getMovie(params['id']).subscribe(movie => {
+        this.movie = movie;
+      });
+    });
+  }
+
 
   ngOnInit() {
   }
